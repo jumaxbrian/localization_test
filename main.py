5e15d6d8adf67ef37ebd3f66dbe96a77c6fa137b
Brian Juma <jumaxbrian@gmail.com>
2019-09-03 09:57:05 +0200

    final

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, flash
 from flask_babel import Babel, _
+# from flask.ext.babel import Babel, _
    
 
 app = Flask(__name__)
+# app.config.from_pyfile('babel.cfg')
 babel = Babel(app)
 
 @app.route("/")
@@ -16,7 +18,9 @@ def text():
 
 @babel.localeselector
 def get_locale():
-    return request.accept_languages.best_match(['en', 'rw'])
+    # return request.accept_languages.best_match(['en', 'rw'])
+    return 'rw'
 
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
     app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
